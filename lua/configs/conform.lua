808a3b69437121309e0e776ec0beb5379bed705f
OxRachid <federermarck@gmail.com>
2024-08-13 04:40:59 +0100

    nvchad neovim c/c++ installation for termux

---
+++ lua/configs/conform.lua
@@ -0,0 +1,36 @@
+local options = {
+    formatters_by_ft = {
+        lua = { "stylua" },
+        c_cpp = { "clang-format" }, -- Hack to force download.
+        c = { "clang_format" },
+        cpp = { "clang_format" },
+    },
+
+    formatters = {
+        clang_format = {
+            prepend_args = {
+                "-style={ \
+                IndentWidth: 4, \
+                TabWidth: 4, \
+                UseTab: Never, \
+                AccessModifierOffset: 0, \
+                IndentAccessModifiers: true, \
+                PackConstructorInitializers: Never,\
+                IndentCaseLabels: true,\
+                NamespaceIndentation: All,\
+        PenaltyBreakString: 1000, \
+        PenaltyExcessCharacter: 1000000, \
+                ColumnLimit: 1200,\
+                AllowShortFunctionsOnASingleLine: Empty}",
+            },
+        },
+    },
+
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 2000,
+        lsp_fallback = true,
+    },
+}
+
+require("conform").setup(options)
